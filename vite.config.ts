030d966cbde32309bec2086cf88ce07b80c7ea73
AfriatMichi <michael.afriat@gmail.com>
2025-07-07 17:21:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/afriatmichi.github.io/2/'
+  base: '/afriatmichi.github.io/'
 }));
